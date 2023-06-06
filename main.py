5f8707d036c73e3daa7585ec53d3af44aba13731
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-06 22:57:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#Exercise Make your own login program
+
+print("My login system")
+print("+++++++++++++++")
+print()
+u = input("Username > ")
+p = input("Password > ")
+if u == "Melany" or "melany" and p == "Melany123":
+  print("Hello bitch! How you doin?")
+elif u == "Michael" or "michael" and p == "Micol123":
+  print("Hi there dummy! Hope you're having a day as bad as mine")
+elif u == "Sara" or "sara" and p == "sarara123":
+  print("Hey! Been trying to meet you! Must be a devil between us")
+else:
+  print("You are not registered and/or the password is wrong")
+
+#Fix my error
+#season = input("what is your favorite season? ")
+#if season == "spring":
+  #print("Ah! The birds are chirping and flowers blooming.")
+#elif season == "summer":
+  #print("Catch some sun and cool off with a lemonade.")
+#elif season == "autumn":
+#  print("The leaves are changing and the air is crisp. Enjoy!")
+#elif season == "winter":
+#  print("Stay warm by the fire and watch the snow fall.")
+#else: 
+#  print("I don't know that season. Please try again.")
+
+#Common Errors
+#print("SECURE LOGIN")
+#username = input("Username > ")
+#
+#if username == "mark":
+#  print("Welcome Mark!")
+#elif username == "suzanne":
+#  print("Hey there Suzanne!")
+#else:
+#  print("Go away!")
+#
+#print("SECURE LOGIN")
+#username = input("Username > ")
+#password = input("Password > ")
+#
+#if username == "mark" and password == "password":
+#  print("Welcome Mark!")
+#elif username == "suzanne" and password == "Su74nne":
+#  print("Hey there Suzanne!")
+#else:
+#  print("Go away!")
